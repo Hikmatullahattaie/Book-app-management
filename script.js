47bbfbae8bb51dbd7137e4aeb742056520c7ae8a
HikmatullahAttaie <hikmatullahattaie@gmail.com>
2024-11-14 01:35:11 +0430

    Add (AddBook)fun

--- script.js
+++ script.js
@@ -10,7 +10,26 @@ $(document).ready(function () {
   
       loadBooks();
   
-     
+      // افزودن کتاب
+    $('#addBook').click(function () {
+        const name = $('#bookName').val();
+        const description = $('#bookDescription').val();
+        const date = $('#bookDate').val();
+
+        if (name && description && date) {
+
+            const books = getBooks();
+            books.push({ name, description, date });
+            saveBooks(books);
+            loadBooks();
+            clearInputs();
+        } else {
+            // alert('لطفاً تمام فیلدها را پر کنید.');
+            var song = $("#fadd")[0]
+            song.play()
+            $(".toast").show()
+        }
+    });
   
      
   });
